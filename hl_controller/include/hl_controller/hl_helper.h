ded3dbc35d0026ab194e1b4ec79c2c27040349a6
ZJUTongYang <327300724@qq.com>
2018-04-19 10:34:28 +0800

    try

--- hl_controller/include/hl_controller/hl_helper.h
+++ hl_controller/include/hl_controller/hl_helper.h
@@ -1,12 +1,14 @@
 #ifndef _HL_HELPER_
 #define _HL_HELPER_
 
+#include "geometry_msgs/PoseStamped.h"
+
 class hl_helper
 {
 public:
   hl_helper();
   ~hl_helper();
-  
+  void cout_PoseStamped(const geometry_msgs::PoseStamped pose);
 
 };
 #endif
--- hl_controller/src/hl_helper.cpp
+++ hl_controller/src/hl_helper.cpp
@@ -1,5 +1,5 @@
 #include "hl_controller/hl_helper.h"
-
+#include "geometry_msgs/PoseStamped.h"
 
 hl_helper::hl_helper()
 {
@@ -7,4 +7,11 @@ hl_helper::hl_helper()
 
 hl_helper::~hl_helper()
 {
+}
+
+void cout_PoseStamped(const geometry_msgs::PoseStamped pose)
+{
+    std::cout << "[" << pose.pose.position.x << ", " << pose.pose.position.y << ", " << pose.pose.position.z << "], [" 
+    << pose.pose.orientation.x << ", " << pose.pose.orientation.y << ", " << pose.pose.orientation.z << ", " << pose.pose.orientation.w << "]" << std::endl;
+
 }
