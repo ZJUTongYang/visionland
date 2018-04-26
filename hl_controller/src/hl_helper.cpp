4c6139793a080569d802119e5ee7f1af770281af
ZJUTongYang <31404141+ZJUTongYang@users.noreply.github.com>
2018-04-26 13:12:53 +0800

    Add files via upload

--- hl_controller/src/hl_helper.cpp
+++ hl_controller/src/hl_helper.cpp
@@ -9,9 +9,30 @@ hl_helper::~hl_helper()
 {
 }
 
-void cout_PoseStamped(const geometry_msgs::PoseStamped pose)
+void hl_helper::cout_PoseStamped(const std::string foreword, const geometry_msgs::PoseStamped& pose, bool show_pos, bool show_quaternion)
 {
-    std::cout << "[" << pose.pose.position.x << ", " << pose.pose.position.y << ", " << pose.pose.position.z << "], [" 
-    << pose.pose.orientation.x << ", " << pose.pose.orientation.y << ", " << pose.pose.orientation.z << ", " << pose.pose.orientation.w << "]" << std::endl;
-
+    std::cout << foreword;
+    if(show_pos)
+    {
+    std::cout << ": [" 
+              << pose.pose.position.x 
+              << ", " 
+              << pose.pose.position.y 
+              << ", " 
+              << pose.pose.position.z 
+              << "]";
+    }
+    if(show_quaternion)
+    {
+        std::cout << ", [" 
+                  << pose.pose.orientation.x 
+                  << ", " 
+                  << pose.pose.orientation.y 
+                  << ", " 
+                  << pose.pose.orientation.z 
+                  << ", " 
+                  << pose.pose.orientation.w 
+                  << "]";
+    }
+    std::cout << std::endl;
 }
